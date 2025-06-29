192b689ee8710e3dbfdda308db18bf5f35e598c0
javidev21 <ingjaviersalas@gmail.com>
2025-06-28 19:11:11 -0600

    Comentar configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
+  //This is for deployment for generate the sitemap by astro. But this is not my deployment site.
+  // I use vercel for deployment, so I will not use this or Render
   site: 'https://porfolio.dev/'
 })
